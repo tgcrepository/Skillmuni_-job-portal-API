0fcc33c37ee529ef4f41f604dfd837f176d2ca73
Vidit Goyal <Viditgoyal18@gmail.com>
2025-05-29 15:53:08 +0530

    Get Controller and Job Preference Count API Changes

--- skillmuniwebservice/Controllers/getAllJobsController.cs
+++ skillmuniwebservice/Controllers/getAllJobsController.cs
@@ -13,8 +13,6 @@ namespace m2ostnextservice.Controllers
 {
     public class getAllJobsController : ApiController
     {
-
-
         //public HttpResponseMessage Get(int OID)
         public HttpResponseMessage Get(int UID, int OID)
         {
@@ -22,22 +20,62 @@ namespace m2ostnextservice.Controllers
             List<tbl_job> tbl_Jobs = new List<tbl_job>();
             using (m2ostnextserviceDbContext db = new m2ostnextserviceDbContext())
             {
-
-                tbl_Jobs = db.Database.SqlQuery<tbl_job>("select * from tbl_job where status='A'").ToList();
-                foreach(var item in tbl_Jobs)
+                if (OID > 0)
                 {
-                    item.loc = db.Database.SqlQuery<string>("SELECT name FROM cities ct INNER JOIN tbl_job_location_mapping ctm ON ct.id= ctm.city where ctm.id_job = {0};",item.id_job).ToList();
-                    if (item.applyflag == 0)
+                    tbl_Jobs = db.Database.SqlQuery<tbl_job>("select * from tbl_job where status='A' AND tbl_organization_id='" + OID + "';").ToList();
+                    foreach (var item in tbl_Jobs)
                     {
-                        if (db.Database.SqlQuery<int>("select id_job_log from tbl_job_user_log where id_user={0} and id_job={1}", UID, item.id_job).FirstOrDefault<int>() > 0)
-                            item.applyflag = 1;
+                        item.loc = db.Database.SqlQuery<string>("SELECT name FROM cities ct INNER JOIN tbl_job_location_mapping ctm ON ct.id= ctm.city where ctm.id_job = {0};", item.id_job).ToList();
+                        if (item.applyflag == 0)
+                        {
+                            if (db.Database.SqlQuery<int>("select id_job_log from tbl_job_user_log where id_user={0} and id_job={1}", UID, item.id_job).FirstOrDefault<int>() > 0)
+                                item.applyflag = 1;
+                        }
+                    }
+                }
+                else
+                {
+                    tbl_Jobs = db.Database.SqlQuery<tbl_job>("select * from tbl_job where status='A'").ToList();
+                    foreach (var item in tbl_Jobs)
+                    {
+                        item.loc = db.Database.SqlQuery<string>("SELECT name FROM cities ct INNER JOIN tbl_job_location_mapping ctm ON ct.id= ctm.city where ctm.id_job = {0};", item.id_job).ToList();
+                        if (item.applyflag == 0)
+                        {
+                            if (db.Database.SqlQuery<int>("select id_job_log from tbl_job_user_log where id_user={0} and id_job={1}", UID, item.id_job).FirstOrDefault<int>() > 0)
+                                item.applyflag = 1;
+                        }
                     }
                 }
 
-                
+
             }
             return Request.CreateResponse(HttpStatusCode.OK, tbl_Jobs);
         }
 
+        [HttpGet]
+        public IHttpActionResult JobPreferenceCount(int UID)
+        {
+            int PreferenceCount = 0;
+            try
+            {
+
+                using (m2ostnextserviceDbContext db = new m2ostnextserviceDbContext())
+                {
+                    PreferenceCount = db.Database.SqlQuery<int>("select COUNT(*) from  tbl_user_job_preferences where id_user={0} ", UID).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            // Create response object
+            var result = new
+            {
+                JobPreferenceCount = PreferenceCount
+            };
+
+            return Ok(result);
+        }
     }
 }
